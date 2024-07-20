876e8270e9da84407c214c19083e4e6c785ca89a
s0obang <tnqls40168@gmail.com>
2024-07-20 18:38:42 +0900

    first commit

---
+++ src/main/java/com/example/metalog/MetalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.metalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/metalog/MetalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
