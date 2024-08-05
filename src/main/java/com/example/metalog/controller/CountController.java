0ae01f3fcbc4785ca142fa0286283be17a04565e
rumi8048 <20231367@sungshin.ac.kr>
2024-08-05 21:27:32 +0900

    마이페이지

--- src/main/java/com/example/metalog/controller/CountController.java
+++ src/main/java/com/example/metalog/controller/CountController.java
@@ -1,2 +1,27 @@
-package com.example.metalog.controller;public class CountController {
+package com.example.metalog.controller;
+
+import com.example.metalog.config.CustomUserDetails;
+import com.example.metalog.dto.CountResponseDTO;
+import com.example.metalog.service.CountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/counts")
+@RequiredArgsConstructor
+public class CountController {
+
+    private final CountService countService;
+
+    @GetMapping
+    public ResponseEntity<CountResponseDTO> getCounts(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        Long userId = userDetails.getId();
+        CountResponseDTO countResponseDTO = countService.getCounts(userId);
+        return ResponseEntity.ok(countResponseDTO);
+    }
 }
--- src/main/java/com/example/metalog/service/CountService.java
+++ src/main/java/com/example/metalog/service/CountService.java
@@ -1,2 +1,24 @@
-package com.example.metalog.service;public class CountService {
+package com.example.metalog.service;
+
+import com.example.metalog.dto.CountResponseDTO;
+import com.example.metalog.repository.CircumstanceRepository;
+import com.example.metalog.repository.GoalRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CountService {
+
+    private final CircumstanceRepository circumstanceRepository;
+    private final GoalRepository goalRepository;
+
+    public CountResponseDTO getCounts(Long userId) {
+        Long circumstancesCount = circumstanceRepository.countByUserId(userId);
+        Long goalsCount = goalRepository.countByUserId(userId);
+        return CountResponseDTO.builder()
+                .circumstancesCount(circumstancesCount)
+                .goalsCount(goalsCount)
+                .build();
+    }
 }
